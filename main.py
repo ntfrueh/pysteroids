5c12adbb4de7e2104f5a06f164148566bde45a39
ntfrueh <ntfrueh@gmail.com>
2025-04-29 18:22:39 -0400

    Basic movement without groups

--- main.py
+++ main.py
@@ -3,20 +3,38 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+import player
 
 
 def main():
-    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    game_clock = pygame.time.Clock()
+    player_object = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    player_object.containers = (updatable, drawable)
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill(color="black")
+        player_object.draw(screen)
+        player_object.update(dt)
         pygame.display.flip()
+        time_passed = game_clock.tick(60)
+        dt = time_passed / 1000
+
 
 if __name__ == "__main__":
     main()
